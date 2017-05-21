bf36a6e58d265e3a75525c7bf9d0c91e9a18f3a8
Michael Li <lijingru931102@gmail.com>
2017-05-21 13:27:28 -0400

    data visualization

--- test2.py
+++ test2.py
@@ -69,3 +69,18 @@ ax.view_init(10,-60)
 plt.show()
 
 
+# faces dataset
+from sklearn.datasets import fetch_olivetti_faces
+faces = fetch_olivetti_faces()
+
+# set up the figure
+fig = plt.figure(figsize = (6,6)) 	#figure size in inches
+fig.subplots_adjust(left = 0, right = 1, bottom = 0, top = 1, hspace = 0.05, wspace = 0.05)
+
+# plot the faces
+for i in range(64):
+	ax = fig.add_subplot(8,8,i+1,xticks = [], yticks = [])
+	ax.imshow(faces.images[i], cmap = plt.cm.binary, interpolation = 'nearest')
+
+plt.show()
+
